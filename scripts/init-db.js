39ac2ad427e912c81b99f229e7b7d9ba93c8a7f3
AguComba <agustinlogin@gmail.com>
2025-01-20 18:08:51 -0300

    ruta dinamica en las migraciones

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -1,33 +1,33 @@
-import fs from "fs"
-import path from "path"
-import { connection } from "../config/db.config.js"
+import fs from 'fs'
+import path from 'path'
+import { connection } from '../config/db.config.js'
 const __dirname = path.resolve()
 
 const runSQLFile = async (filePath) => {
-    const query = fs.readFileSync(filePath, "utf-8")
-    try {
-        await connection.query(query)
-        console.log(`Successfully ran ${filePath}`)
-    } catch (error) {
-        console.error(`Error running ${filePath}:`, error)
-    }
+  const query = fs.readFileSync(filePath, 'utf-8')
+  try {
+    await connection.query(query)
+    console.log(`Successfully ran ${filePath}`)
+  } catch (error) {
+    console.error(`Error running ${filePath}:`, error)
+  }
 }
 
 const runMigrations = async () => {
-    const migrationsDir = "C:/Users/agust/padel-back/db/migrations"
-    const files = fs.readdirSync(migrationsDir)
-    for (const file of files) {
-        const filePath = path.join(migrationsDir, file)
-        await runSQLFile(filePath)
-    }
+  const migrationsDir = `${__dirname}/db/migrations`
+  const files = fs.readdirSync(migrationsDir)
+  for (const file of files) {
+    const filePath = path.join(migrationsDir, file)
+    await runSQLFile(filePath)
+  }
 }
 
 runMigrations()
-    .then(() => {
-        console.log("All migrations have been run")
-        process.exit(0)
-    })
-    .catch((error) => {
-        console.error("Error running migrations:", error)
-        process.exit(1)
-    })
+  .then(() => {
+    console.log('All migrations have been run')
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.error('Error running migrations:', error)
+    process.exit(1)
+  })
