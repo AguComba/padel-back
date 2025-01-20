a3764273910e1556cde30af270b519310f32ff10
AguComba <agustinlogin@gmail.com>
2025-01-20 17:00:21 -0300

    cambios en la configuracion de app

--- config/app.config.js
+++ config/app.config.js
@@ -1,10 +1,11 @@
-process.loadEnvFile()
-export const {
-    PORT,
-    ENVAIROMENT,
-    DB_HOST,
-    DB_PORT,
-    DB_USER,
-    DB_PASS,
-    DB_SCHEMA,
-} = process.env
+try {
+  // Intentar cargar variables desde un archivo .env, solo en desarrollo
+  process.loadEnvFile()
+  console.log('.env cargado correctamente.')
+} catch (err) {
+  console.log('Archivo .env no encontrado o no es necesario. Usando variables del entorno.')
+}
+
+export const { PORT, ENVAIROMENT, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_SCHEMA } = process.env
+
+console.log(DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_SCHEMA, PORT, ENVAIROMENT)
