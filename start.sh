0f67180c6e00accc72741584f24cbfd186dad11c
AguComba <agustinlogin@gmail.com>
2025-01-20 17:54:00 -0300

    cambio script start

--- start.sh
+++ start.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-echo "Ejecutando migraciones..."
-node --run migrations
+# Archivo que marca si las migraciones y seeders ya se ejecutaron
+FLAG_FILE=".setup_done"
 
-echo "Ejecutando seeders..."
-node --run seeders
+# Comprueba si el archivo de marca existe
+if [ ! -f "$FLAG_FILE" ]; then
+    echo "Primera ejecución: Ejecutando migraciones y seeders..."
+
+    echo "Ejecutando migraciones..."
+    node --run migrations
+
+    echo "Ejecutando seeders..."
+    node --run seeders
+
+    # Crea el archivo de marca para evitar ejecutar migraciones y seeders de nuevo
+    touch "$FLAG_FILE"
+
+    echo "Migraciones y seeders completados."
+else
+    echo "Migraciones y seeders ya se ejecutaron previamente. Saltando..."
+fi
 
 echo "Iniciando la aplicación..."
 npm start
