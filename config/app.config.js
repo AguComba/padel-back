ed2dce74b3fe4db45d550df3c1a236745e0c6b06
agust1n <acomba@coopmorteros.coop>
2025-01-27 18:40:16 -0300

    prueba railway

--- config/app.config.js
+++ config/app.config.js
@@ -1,9 +1,10 @@
 try {
-  // Intentar cargar variables desde un archivo .env, solo en desarrollo
-  process.loadEnvFile()
-  console.log('.env cargado correctamente.')
+    // Intentar cargar variables desde un archivo .env, solo en desarrollo
+    process.loadEnvFile()
+    console.log('.env cargado correctamente.')
 } catch (err) {
-  console.log('Archivo .env no encontrado o no es necesario. Usando variables del entorno.')
+    console.log('Archivo .env no encontrado o no es necesario. Usando variables del entorno.')
 }
 
+console.log(PORT, ENVAIROMENT, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_SCHEMA, SECRET_JWT_KEY)
 export const { PORT, ENVAIROMENT, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_SCHEMA, SECRET_JWT_KEY } = process.env
