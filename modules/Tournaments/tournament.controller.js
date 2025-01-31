c19a418b729edd6678a01132f7a06a61d82b0ec0
agust1n <acomba@coopmorteros.coop>
2025-01-30 21:03:06 -0300

    feat: actualizacion de creacion de torneos

--- modules/Tournaments/tournament.controller.js
+++ modules/Tournaments/tournament.controller.js
@@ -3,40 +3,36 @@ import { TournamentSchema } from '../../schemas/Tournament.schema.js'
 import { TournamentModel } from './tournament.model.js'
 import { parse } from '@formkit/tempo'
 
-const parseDate = (dateString) => {
-  const [day, month, year] = dateString.split('/')
-  return new Date(`${year}-${month}-${day}`)
-}
-
 const parseDateTempo = (dateString, time = false) => {
-  let data = null
-  if (time) {
-    data = parse(dateString, 'DD/MM/YYYY HH:mm:ss', 'es')
+    let data = null
+    if (time) {
+        data = parse(dateString, 'DD/MM/YYYY HH:mm:ss', 'es')
+        return data
+    }
+    data = parse(dateString, 'DD/MM/YYYY', 'es')
     return data
-  }
-  data = parse(dateString, 'DD/MM/YYYY', 'es')
-  return data
 }
 
 export const createTournament = async (req, res) => {
-  try {
-    const { user = false } = req.session
-    const tournament = req.body
-    if (!hasRole(user, ['admin', 'superAdmin'])) {
-      return res.status(401).json({ message: 'No tienes permisos para acceder a este recurso' })
+    try {
+        const { user = false } = req.session
+        const tournament = req.body
+        if (!hasRole(user, ['admin', 'superAdmin'])) {
+            return res.status(401).json({ message: 'No tienes permisos para acceder a este recurso' })
+        }
+
+        tournament.date_start = parseDateTempo(tournament.date_start)
+        tournament.date_end = parseDateTempo(tournament.date_end)
+        tournament.date_inscription_start = parseDateTempo(tournament.date_inscription_start, true)
+        tournament.date_inscription_end = parseDateTempo(tournament.date_inscription_end, true)
+
+        const validTournament = TournamentSchema.parse(tournament)
+        validTournament.user_created = user.id
+        console.log(validTournament)
+
+        const tournamentCreated = await TournamentModel.create(validTournament)
+        res.status(200).json(tournamentCreated)
+    } catch (error) {
+        return res.status(400).json(error.message)
     }
-
-    tournament.date_start = parseDateTempo(tournament.date_start)
-    tournament.date_inscription_start = parseDateTempo(tournament.date_inscription_start, true)
-    tournament.date_inscription_end = parseDateTempo(tournament.date_inscription_end, true)
-
-    const validTournament = TournamentSchema.parse(tournament)
-    validTournament.user_created = user.id
-    console.log(validTournament)
-
-    const tournamentCreated = await TournamentModel.create(validTournament)
-    res.status(200).json(tournamentCreated)
-  } catch (error) {
-    return res.status(400).json(error.message)
-  }
 }
--- modules/Tournaments/tournament.model.js
+++ modules/Tournaments/tournament.model.js
@@ -1,48 +1,71 @@
 import { handleTransaction } from '../../utils/transactions.js'
 
 export class TournamentModel {
-  static async create(tournament) {
-    return await handleTransaction(async (connection) => {
-      // Desestructurar categorías del objeto torneo.
-      const { categories, ...tournamentData } = tournament
+    static async create(tournament) {
+        return await handleTransaction(async (connection) => {
+            // Desestructurar categorías del objeto torneo.
+            const { categories, clubs, ...tournamentData } = tournament
 
-      // Crear el torneo.
-      const [rows] = await connection.query(
-        `INSERT INTO tournaments 
-        (name, id_federation, date_start, date_inscription_start, date_inscription_end, max_couples, afiliation_required,
+            // Crear el torneo.
+            const [rows] = await connection.query(
+                `INSERT INTO tournaments 
+        (name, id_federation, date_start, date_end, date_inscription_start, date_inscription_end, max_couples, gender,afiliation_required,
         type_tournament, user_created, user_updated) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-        [
-          tournamentData.name,
-          tournamentData.id_federation,
-          tournamentData.date_start,
-          tournamentData.date_inscription_start,
-          tournamentData.date_inscription_end,
-          tournamentData.max_couples,
-          tournamentData.afiliation_required,
-          tournamentData.type_tournament,
-          tournamentData.user_created,
-          tournamentData.user_created,
-        ],
-      )
+        VALUES (?, ?,?,?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+                [
+                    tournamentData.name,
+                    tournamentData.id_federation,
+                    tournamentData.date_start,
+                    tournamentData.date_end,
+                    tournamentData.date_inscription_start,
+                    tournamentData.date_inscription_end,
+                    tournamentData.max_couples,
+                    tournamentData.gender,
+                    tournamentData.afiliation_required,
+                    tournamentData.type_tournament,
+                    tournamentData.user_created,
+                    tournamentData.user_created
+                ]
+            )
 
-      if (rows.affectedRows === 0) {
-        throw new Error('No se pudo crear el torneo')
-      }
+            if (rows.affectedRows === 0) {
+                throw new Error('No se pudo crear el torneo')
+            }
 
-      const idTournament = rows.insertId
+            const idTournament = rows.insertId
 
-      // Crear las categorías asociadas al torneo.
-      const categoriesData = categories.map((category) => [idTournament, category.id_category, category.genere, tournamentData.user_created])
+            // Crear las categorías asociadas al torneo.
+            const categoriesData = categories.map((category) => [
+                idTournament,
+                category.id_category,
+                tournamentData.user_created
+            ])
+            const clubsData = clubs.map((club) => [
+                idTournament,
+                club.id_club,
+                club.main_club,
+                tournamentData.user_created
+            ])
 
-      const [rowsCategories] = await connection.query(`INSERT INTO tournament_categories(id_tournament, id_category, gender, user_created) VALUES ?`, [categoriesData])
+            const [rowsCategories] = await connection.query(
+                `INSERT INTO tournament_categories(id_tournament, id_category, user_created) VALUES ?`,
+                [categoriesData]
+            )
 
-      if (rowsCategories.affectedRows === 0) {
-        throw new Error('No se pudieron crear las categorías del torneo')
-      }
+            if (rowsCategories.affectedRows === 0) {
+                throw new Error('No se pudieron crear las categorías del torneo')
+            }
 
-      // Retornar el torneo creado con sus categorías.
-      return { id: idTournament, ...tournament, categories }
-    })
-  }
+            const [rowsTournamentClub] = await connection.query(
+                `INSERT INTO tournament_clubs(id_tournament, id_club, main_club, user_created) VALUES ?`,
+                [clubsData]
+            )
+
+            if (rowsTournamentClub.affectedRows === 0) {
+                throw new Error('No se pudieron crear los clubes del torneo')
+            }
+            // Retornar el torneo creado con sus categorías.
+            return { id: idTournament, ...tournament, categories, clubs }
+        })
+    }
 }
