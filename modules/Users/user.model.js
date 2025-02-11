14a9b6749c6f0c14339f3b299c2ed3b0aef31802
agust1n <acomba@coopmorteros.coop>
2025-02-11 17:46:48 -0300

    fix: Se agrega distinct a la consulta de usuarios

--- modules/Users/user.model.js
+++ modules/Users/user.model.js
@@ -4,7 +4,7 @@ export class UserModel {
     static async searchAllUsers() {
         try {
             const users = await executeQuery(
-                `SELECT name, last_name, cell_phone, email, number_document, gender, type_user, afiliation FROM users u
+                `SELECT DISTINCT name, last_name, cell_phone, email, number_document, gender, type_user, afiliation FROM users u
                 LEFT JOIN players p ON p.id_user = u.id
                 WHERE u.status = 1`
             )
