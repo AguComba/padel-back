678e9e4ee557fc892f675c8a4f8dd13f78469b79
agust1n <acomba@coopmorteros.coop>
2025-02-14 08:46:51 -0300

    cambios para validar inscripcion por genero y categoria

--- modules/Inscriptions/inscriptions.controller.js
+++ modules/Inscriptions/inscriptions.controller.js
@@ -5,123 +5,141 @@ import { TournamentModel } from '../Tournaments/tournament.model.js'
 import { InscriptionModel } from './inscriptions.model.js'
 
 const isValidPlayer = async (player, id_tournament, gender) => {
-  const aceptedTournament = await TournamentModel.searchTournamentByCategoryPlayer(player.id_cat, id_tournament, gender)
-  return !!aceptedTournament
+    const aceptedTournament = await TournamentModel.searchTournamentByCategoryPlayer(
+        player.id_cat,
+        id_tournament,
+        gender
+    )
+    return !!aceptedTournament
 }
 
 const isInscriptedPlayer = async (player, id_tournament) => {
-  const inscriptionStatus = await InscriptionModel.searchInscriptionByPlayerId(player.id, id_tournament)
-  return inscriptionStatus || false
+    const inscriptionStatus = await InscriptionModel.searchInscriptionByPlayerId(player.id, id_tournament)
+    return inscriptionStatus || false
 }
 
 export const createInscriptionCouple = async (req, res) => {
-  try {
-    const inscription = req.body
-    const { user = false } = req.session
-    if (!isPlayer(user)) {
-      return res.status(403).json({ message: 'El usuario debe ser un jugador para inscribirse' })
+    try {
+        const inscription = req.body
+        const { user = false } = req.session
+        if (!isPlayer(user)) {
+            return res.status(403).json({ message: 'El usuario debe ser un jugador para inscribirse' })
+        }
+
+        const player = await PlayerModel.searchByIdUserAfiliated(user.id)
+        if (!player) {
+            return res.status(401).json({ message: 'Usted no esta afilidado.' })
+        }
+
+        if (!(await isValidPlayer(player, inscription.id_tournament, user.gender))) {
+            return res.status(401).json({
+                message: 'Usted no califica para jugar ningun torneo vigente'
+            })
+        }
+
+        const statusInscriptionPlayer = await isInscriptedPlayer(player, inscription.id_tournament)
+        if (statusInscriptionPlayer) {
+            return res.status(400).json({
+                message: `Usted ya es encuentra inscripto con ${
+                    statusInscriptionPlayer.id_titular == player.id
+                        ? statusInscriptionPlayer.companero
+                        : statusInscriptionPlayer.titular
+                }`
+            })
+        }
+
+        inscription.id_player_1 = player.id
+        inscription.id_club = player.id_club
+        inscription.user_created = user.id
+
+        const categoryTournamentPlayer1 = await TournamentModel.searchTournamentByCategoryPlayer(
+            player.id_cat,
+            inscription.id_tournament,
+            user.gender
+        )
+
+        inscription.id_category = categoryTournamentPlayer1.id_category
+        const validInscription = InscriptionSchema.safeParse(inscription)
+        if (!validInscription.success) {
+            return res.status(400).json(validInscription.error.errors)
+        }
+
+        const player2 = await PlayerModel.searchById(validInscription.data.id_player_2)
+        if (!player2.afiliation) {
+            return res.status(400).json({ message: 'Tu compañero no esta afiliado.' })
+        }
+
+        if (!(await isValidPlayer(player2, validInscription.data.id_tournament))) {
+            return res.status(401).json({
+                message: 'Tu compañero no califica para jugar ningun torneo vigente'
+            })
+        }
+
+        const statusInscriptionPlayer2 = await isInscriptedPlayer(player2, inscription.id_tournament, player2.gender)
+        if (statusInscriptionPlayer2) {
+            return res.status(400).json({
+                message: `Su compañero ya se encuentra inscripto con ${
+                    statusInscriptionPlayer2.id_titular == player2.id
+                        ? statusInscriptionPlayer2.companero
+                        : statusInscriptionPlayer2.titular
+                }`
+            })
+        }
+        const categoryTournamentPlayer2 = await TournamentModel.searchTournamentByCategoryPlayer(
+            player2.id_cat,
+            validInscription.data.id_tournament,
+            player.gender
+        )
+
+        if (categoryTournamentPlayer1.id_category !== categoryTournamentPlayer2.id_category) {
+            return res.status(400).json({
+                message: `Usted califica para jugar en ${categoryTournamentPlayer1.categoria} y su compañero en ${categoryTournamentPlayer2.categoria}. No pueden jugar juntos.`
+            })
+        }
+        const inscriptionCrated = await InscriptionModel.create(validInscription.data)
+
+        res.status(200).json(inscriptionCrated)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: 'Ocurrio un error inesperado' })
     }
-
-    const player = await PlayerModel.searchByIdUserAfiliated(user.id)
-    if (!player) {
-      return res.status(401).json({ message: 'Usted no esta afilidado.' })
-    }
-
-    if (!(await isValidPlayer(player, inscription.id_tournament, user.gender))) {
-      return res.status(401).json({
-        message: 'Usted no califica para jugar ningun torneo vigente',
-      })
-    }
-
-    const statusInscriptionPlayer = await isInscriptedPlayer(player, inscription.id_tournament)
-    if (statusInscriptionPlayer) {
-      return res.status(400).json({
-        message: `Usted ya es encuentra inscripto con ${statusInscriptionPlayer.id_titular == player.id ? statusInscriptionPlayer.companero : statusInscriptionPlayer.titular}`,
-      })
-    }
-
-    inscription.id_player_1 = player.id
-    inscription.id_club = player.id_club
-    inscription.user_created = user.id
-
-    const categoryTournamentPlayer1 = await TournamentModel.searchTournamentByCategoryPlayer(player.id_cat, inscription.id_tournament, user.gender)
-
-    inscription.id_category = categoryTournamentPlayer1.id_category
-    const validInscription = InscriptionSchema.safeParse(inscription)
-    if (!validInscription.success) {
-      return res.status(400).json(validInscription.error.errors)
-    }
-
-    const player2 = await PlayerModel.searchById(validInscription.data.id_player_2)
-    if (!player2.afiliation) {
-      return res.status(400).json({ message: 'Tu compañero no esta afiliado.' })
-    }
-
-    if (!(await isValidPlayer(player2, validInscription.data.id_tournament))) {
-      return res.status(401).json({
-        message: 'Tu compañero no califica para jugar ningun torneo vigente',
-      })
-    }
-
-    console.log(player2)
-    return res.status(200).json(validInscription.data)
-    const statusInscriptionPlayer2 = await isInscriptedPlayer(player2, inscription.id_tournament, player2.gender)
-    if (statusInscriptionPlayer2) {
-      return res.status(400).json({
-        message: `Su compañero ya se encuentra inscripto con ${statusInscriptionPlayer2.id_titular == player2.id ? statusInscriptionPlayer2.companero : statusInscriptionPlayer2.titular}`,
-      })
-    }
-    const categoryTournamentPlayer2 = await TournamentModel.searchTournamentByCategoryPlayer(player2.id_cat, validInscription.data.id_tournament)
-
-    if (categoryTournamentPlayer1.id_category !== categoryTournamentPlayer2.id_category) {
-      return res.status(400).json({
-        message: `Usted califica para jugar en ${categoryTournamentPlayer1.categoria} y su compañero en ${categoryTournamentPlayer2.categoria}. No pueden jugar juntos.`,
-      })
-    }
-    const inscriptionCrated = await InscriptionModel.create(validInscription.data)
-
-    res.status(200).json(inscriptionCrated)
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({ message: 'Ocurrio un error inesperado' })
-  }
 }
 
 export const getInscriptions = async (req, res) => {
-  try {
-    const { user = false } = req.session
-    const { id_tournament = false } = req.params
-    const { category = false, suplentes = false } = req.query
+    try {
+        const { user = false } = req.session
+        const { id_tournament = false } = req.params
+        const { category = false, suplentes = false } = req.query
 
-    if (!hasRole(user, ['admin', 'superAdmin'])) {
-      return res.status(401).json({
-        message: 'El usuario no tiene permisos para acceder a este recurso',
-      })
+        if (!hasRole(user, ['admin', 'superAdmin'])) {
+            return res.status(401).json({
+                message: 'El usuario no tiene permisos para acceder a este recurso'
+            })
+        }
+
+        const inscriptions = await InscriptionModel.search(id_tournament)
+
+        const filteredInscriptions = inscriptions.filter((inscription) => {
+            if (category && suplentes) {
+                return inscription.id_category == category && inscription.status == suplentes
+            }
+            if (category) {
+                return inscription.id_category == category
+            }
+            if (suplentes == 1) {
+                return inscription.status == suplentes
+            }
+            if (suplentes == 2) {
+                return inscription.status == suplentes
+            }
+            return inscription
+        })
+
+        return res.status(200).json(filteredInscriptions)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            message: 'Ocurrio un error en el sistema'
+        })
     }
-
-    const inscriptions = await InscriptionModel.search(id_tournament)
-
-    const filteredInscriptions = inscriptions.filter((inscription) => {
-      if (category && suplentes) {
-        return inscription.id_category == category && inscription.status == suplentes
-      }
-      if (category) {
-        return inscription.id_category == category
-      }
-      if (suplentes == 1) {
-        return inscription.status == suplentes
-      }
-      if (suplentes == 2) {
-        return inscription.status == suplentes
-      }
-      return inscription
-    })
-
-    return res.status(200).json(filteredInscriptions)
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({
-      message: 'Ocurrio un error en el sistema',
-    })
-  }
 }
--- modules/Tournaments/tournament.model.js
+++ modules/Tournaments/tournament.model.js
@@ -2,68 +2,87 @@ import { executeQuery } from '../../utils/executeQuery.js'
 import { handleTransaction } from '../../utils/transactions.js'
 
 export class TournamentModel {
-  static async create(tournament) {
-    return await handleTransaction(async (connection) => {
-      // Desestructurar categorías del objeto torneo.
-      const { categories, clubs, ...tournamentData } = tournament
+    static async create(tournament) {
+        return await handleTransaction(async (connection) => {
+            // Desestructurar categorías del objeto torneo.
+            const { categories, clubs, ...tournamentData } = tournament
 
-      // Crear el torneo.
-      const [rows] = await connection.query(
-        `INSERT INTO tournaments 
+            // Crear el torneo.
+            const [rows] = await connection.query(
+                `INSERT INTO tournaments 
         (name, id_federation, date_start, date_end, date_inscription_start, date_inscription_end, max_couples, gender,afiliation_required,
         type_tournament, user_created, user_updated) 
         VALUES (?, ?,?,?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-        [
-          tournamentData.name,
-          tournamentData.id_federation,
-          tournamentData.date_start,
-          tournamentData.date_end,
-          tournamentData.date_inscription_start,
-          tournamentData.date_inscription_end,
-          tournamentData.max_couples,
-          tournamentData.gender,
-          tournamentData.afiliation_required,
-          tournamentData.type_tournament,
-          tournamentData.user_created,
-          tournamentData.user_created,
-        ],
-      )
+                [
+                    tournamentData.name,
+                    tournamentData.id_federation,
+                    tournamentData.date_start,
+                    tournamentData.date_end,
+                    tournamentData.date_inscription_start,
+                    tournamentData.date_inscription_end,
+                    tournamentData.max_couples,
+                    tournamentData.gender,
+                    tournamentData.afiliation_required,
+                    tournamentData.type_tournament,
+                    tournamentData.user_created,
+                    tournamentData.user_created
+                ]
+            )
 
-      if (rows.affectedRows === 0) {
-        throw new Error('No se pudo crear el torneo')
-      }
+            if (rows.affectedRows === 0) {
+                throw new Error('No se pudo crear el torneo')
+            }
 
-      const idTournament = rows.insertId
+            const idTournament = rows.insertId
 
-      // Crear las categorías asociadas al torneo.
-      const categoriesData = categories.map((category) => [idTournament, category.id_category, tournamentData.user_created])
-      const clubsData = clubs.map((club) => [idTournament, club.id_club, club.main_club, tournamentData.user_created])
+            // Crear las categorías asociadas al torneo.
+            const categoriesData = categories.map((category) => [
+                idTournament,
+                category.id_category,
+                tournamentData.user_created
+            ])
+            const clubsData = clubs.map((club) => [
+                idTournament,
+                club.id_club,
+                club.main_club,
+                tournamentData.user_created
+            ])
 
-      const [rowsCategories] = await connection.query(`INSERT INTO tournament_categories(id_tournament, id_category, user_created) VALUES ?`, [categoriesData])
+            const [rowsCategories] = await connection.query(
+                `INSERT INTO tournament_categories(id_tournament, id_category, user_created) VALUES ?`,
+                [categoriesData]
+            )
 
-      if (rowsCategories.affectedRows === 0) {
-        throw new Error('No se pudieron crear las categorías del torneo')
-      }
+            if (rowsCategories.affectedRows === 0) {
+                throw new Error('No se pudieron crear las categorías del torneo')
+            }
 
-      const [rowsTournamentClub] = await connection.query(`INSERT INTO tournament_clubs(id_tournament, id_club, main_club, user_created) VALUES ?`, [clubsData])
+            const [rowsTournamentClub] = await connection.query(
+                `INSERT INTO tournament_clubs(id_tournament, id_club, main_club, user_created) VALUES ?`,
+                [clubsData]
+            )
 
-      if (rowsTournamentClub.affectedRows === 0) {
-        throw new Error('No se pudieron crear los clubes del torneo')
-      }
+            if (rowsTournamentClub.affectedRows === 0) {
+                throw new Error('No se pudieron crear los clubes del torneo')
+            }
 
-      const [rowsTournamentAmoutn] = await connection.query(`INSERT INTO amounts(type_amount, amount, id_tournament) VALUES (?,?,?)`, [tournamentData.type_amount, tournamentData.amount, idTournament])
+            const [rowsTournamentAmoutn] = await connection.query(
+                `INSERT INTO amounts(type_amount, amount, id_tournament) VALUES (?,?,?)`,
+                [tournamentData.type_amount, tournamentData.amount, idTournament]
+            )
 
-      if (rowsTournamentAmoutn.affectedRows === 0) {
-        throw new Error('No se pudieron crear los clubes del torneo')
-      }
-      // Retornar el torneo creado con sus categorías.
-      return { id: idTournament, ...tournament, categories, clubs }
-    })
-  }
+            if (rowsTournamentAmoutn.affectedRows === 0) {
+                throw new Error('No se pudieron crear los clubes del torneo')
+            }
+            // Retornar el torneo creado con sus categorías.
+            return { id: idTournament, ...tournament, categories, clubs }
+        })
+    }
 
-  static async search() {
-    try {
-      const rows = await executeQuery(`SELECT t.id, t.name, date_start, date_end, date_inscription_start, date_inscription_end, t.max_couples,
+    static async search() {
+        try {
+            const rows =
+                await executeQuery(`SELECT t.id, t.name, date_start, date_end, date_inscription_start, date_inscription_end, t.max_couples,
                     t.gender, club.name as club, c.name as ciudad, a.amount
                     FROM tournaments t
                     INNER JOIN amounts a ON a.id_tournament = t.id
@@ -73,15 +92,16 @@ export class TournamentModel {
                     WHERE t.status = 1 AND t_club.main_club = 1
                     AND CURDATE() <= t.date_end
                 `)
-      return rows
-    } catch (error) {
-      throw new Error(error)
+            return rows
+        } catch (error) {
+            throw new Error(error)
+        }
     }
-  }
 
-  static async searchById(id_tournament) {
-    try {
-      const tournament = await executeQuery(`SELECT t.id, t.name, date_start, date_end, date_inscription_start, date_inscription_end, t.max_couples,
+    static async searchById(id_tournament) {
+        try {
+            const tournament =
+                await executeQuery(`SELECT t.id, t.name, date_start, date_end, date_inscription_start, date_inscription_end, t.max_couples,
                     t.gender, club.name as club, c.name as ciudad, a.amount, t.afiliation_required
                     FROM tournaments t
                     INNER JOIN amounts a ON a.id_tournament = t.id
@@ -90,30 +110,30 @@ export class TournamentModel {
                     INNER JOIN cities c ON club.id_city = c.id
                     WHERE t.status = 1 AND t_club.main_club = 1 AND t.id = ?
                     `)
-      return tournament.shift()
-    } catch (error) {
-      throw new Error()
+            return tournament.shift()
+        } catch (error) {
+            throw new Error()
+        }
     }
-  }
 
-  static async searchCategories(id) {
-    try {
-      const rows = await executeQuery(
-        `SELECT c.name, c.level FROM tournament_categories tc
+    static async searchCategories(id) {
+        try {
+            const rows = await executeQuery(
+                `SELECT c.name, c.level FROM tournament_categories tc
                 INNER JOIN categories c ON c.id = tc.id_category
                 WHERE id_tournament = ?`,
-        [id],
-      )
-      return rows
-    } catch (error) {
-      throw new Error(error)
+                [id]
+            )
+            return rows
+        } catch (error) {
+            throw new Error(error)
+        }
     }
-  }
 
-  static async searchTournamentByCategoryPlayer(category, tournament_id, gender) {
-    try {
-      const tournament = await executeQuery(
-        `SELECT t.id, t.name, date_start, date_end, date_inscription_start, date_inscription_end, 
+    static async searchTournamentByCategoryPlayer(category, tournament_id, gender) {
+        try {
+            const tournament = await executeQuery(
+                `SELECT t.id, t.name, date_start, date_end, date_inscription_start, date_inscription_end, 
           t.max_couples, t.gender, club.name as club, c.name as ciudad, 
           cat.id as id_category, cat.name as categoria
           FROM tournaments t
@@ -130,9 +150,9 @@ export class TournamentModel {
             AND cat_res.category_gender = t.gender
             AND cat_res.authorized_category_gender = ?
                 `,
-        [category, tournament_id, gender],
-      )
-      return tournament.shift()
-    } catch (error) {}
-  }
+                [category, tournament_id, gender]
+            )
+            return tournament.shift()
+        } catch (error) {}
+    }
 }
