767677ffedadfd35d7e7eca2bffc2cfa1b1870fe
agust1n <acomba@coopmorteros.coop>
2025-03-08 13:08:28 -0300

    feat: se agrego el metodo para importar a la db el ranking

--- modules/Ranking/ranking.controller.js
+++ modules/Ranking/ranking.controller.js
@@ -1,16 +1,77 @@
-import { isAcceptedUser, isPlayer } from '../../middlewares/permisions.js'
+import XlsxPopulate from 'xlsx-populate'
+import { isAcceptedUser, isAdmin, isPlayer } from '../../middlewares/permisions.js'
 import { RankingModel } from './ranking.model.js'
+import path from 'path'
 
 export const getRanking = async (req, res) => {
-  try {
-    const { user = false } = req.session
-    if (!isAcceptedUser(user)) {
-      return res.status(401).json({ message: 'No tienes permisos para acceder a este recurso' })
-    }
+    try {
+        const { user = false } = req.session
+        if (!isAcceptedUser(user)) {
+            return res.status(401).json({ message: 'No tienes permisos para acceder a este recurso' })
+        }
 
-    const ranking = await RankingModel.search()
-    res.status(200).json(ranking)
-  } catch (error) {
-    res.status(400).json({ message: error.message })
-  }
+        const ranking = await RankingModel.search()
+        res.status(200).json(ranking)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const getIndexColumns = (columns) => {
+    const claves = ['id', 'categoria', 'puntos', 'estado']
+    return claves.reduce((indices, clave) => {
+        indices[clave] = columns.findIndex((col) => col.toLowerCase() === clave.toLowerCase())
+        return indices
+    }, {})
+}
+
+export const importRanking = async (req, res) => {
+    try {
+        const { user = false } = req.session
+        if (!isAdmin(user)) {
+            return res.status(403).json('No esta autorizado para esto.')
+        }
+
+        const __dirname = path.resolve()
+        const excel = await XlsxPopulate.fromFileAsync(`${__dirname}/archivos/IMPAR-CABALLEROS-5TA.xlsx`)
+        const values = excel.sheet('Jugadores').usedRange().value()
+        const { id, puntos, estado } = getIndexColumns(values[0])
+        const { gender = false, year = false, categoria = false } = req.body
+
+        if (!['F', 'X'].includes(gender)) {
+            return res.status(400).json('Genero invalido')
+        }
+        if (!categoria) {
+            return res.status(400).json('Debe enviar la categoria')
+        }
+
+        if (!year) {
+            return res.status(400).json('Debe pasar el año')
+        }
+        // Comienzo a recorrer values despues de la primer fila
+        // id player, puntos, federacion, categoria, estado, año, genero
+        const dataToInsert = values.slice(1).map((row) => ({
+            id_player: row[id],
+            points: row[puntos],
+            id_federation: 1,
+            id_category: categoria,
+            status: row[estado] === 'ASCENDIDO' ? 2 : 1,
+            year: year,
+            gender: gender,
+            user_updated: user.id
+        }))
+
+        const resultDB = await RankingModel.import(dataToInsert)
+
+        return res.status(200).json(resultDB)
+    } catch (error) {
+        return res.status(400).json({ message: error.message })
+    }
+}
+
+export const createRanking = async (req, res) => {
+    try {
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 }
--- modules/Ranking/ranking.model.js
+++ modules/Ranking/ranking.model.js
@@ -1,4 +1,5 @@
 import { executeQuery } from '../../utils/executeQuery.js'
+import { handleTransaction } from '../../utils/transactions.js'
 
 export class RankingModel {
     static async search() {
@@ -16,6 +17,54 @@ export class RankingModel {
             return rows
         } catch (error) {
             throw new Error(error)
+        }
+    }
+
+    static async import(data) {
+        try {
+            return await handleTransaction(async (connection) => {
+                let updated = 0
+                let added = 0
+                for (const rank of data) {
+                    // Buscar si ya existe un registro con los criterios especificados
+                    const [existing] = await connection.query(
+                        `SELECT id FROM ranking 
+                         WHERE id_player = ? AND id_category = ? AND gender = ? AND year = ? 
+                         LIMIT 1`,
+                        [rank.id_player, rank.id_category, rank.gender, rank.year]
+                    )
+
+                    if (existing.length > 0) {
+                        // Si el registro ya existe, lo actualizamos
+                        await connection.query(`UPDATE ranking SET points = ?, user_updated = ? WHERE id = ?`, [
+                            rank.points,
+                            rank.user_updated,
+                            existing[0].id
+                        ])
+                        updated++
+                    } else {
+                        // Si no existe, insertamos un nuevo registro
+                        await connection.query(
+                            `INSERT INTO ranking (id_player, points, id_federation, id_category, status, year, gender, user_updated) 
+                             VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+                            [
+                                rank.id_player,
+                                rank.points,
+                                rank.id_federation,
+                                rank.id_category,
+                                rank.status,
+                                rank.year,
+                                rank.gender,
+                                rank.user_updated
+                            ]
+                        )
+                        added++
+                    }
+                    console.log(updated, added)
+                }
+            })
+        } catch (error) {
+            throw new Error(error.message)
         }
     }
 }
