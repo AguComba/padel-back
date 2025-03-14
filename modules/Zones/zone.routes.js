aed987a8b001e55c3b0098209d4cca7d82f6adb5
agust1n <acomba@coopmorteros.coop>
2025-03-14 15:04:44 -0300

    ruta con validate token

--- modules/Zones/zone.routes.js
+++ modules/Zones/zone.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { generateByCategory } from './zone.controller.js'
+import { validateToken } from '../../middlewares/validateToken.js'
 
 export const zonesRouter = Router()
 
-zonesRouter.post('/', generateByCategory)
+zonesRouter.post('/', validateToken, generateByCategory)
