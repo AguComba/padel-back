bff763473465e96e9e22e4c9c8bb2547a9e5013b
agust1n <acomba@coopmorteros.coop>
2025-04-08 20:35:50 -0300

    correccion en error de ranked al generar zonas

--- modules/Ranking/ranking.routes.js
+++ modules/Ranking/ranking.routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express'
-import { createRanking, getRanking, importRanking } from './ranking.controller.js'
+import { getRanking, importRanking } from './ranking.controller.js'
 import { validateToken } from '../../middlewares/validateToken.js'
 
 export const rankingRouter = Router()
 
 rankingRouter.get('/', validateToken, getRanking)
 rankingRouter.post('/import', validateToken, importRanking)
-rankingRouter.post('/', validateToken, createRanking)
