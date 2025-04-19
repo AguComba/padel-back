eb3e3512cec6d78f45c583baf01ca4ed6c147796
Agustin Comba <acombadev@gmail.com>
2025-04-19 20:35:47 -0300

    se agrega funcion para obtener ranking por jugador

--- modules/Ranking/ranking.routes.js
+++ modules/Ranking/ranking.routes.js
@@ -1,8 +1,9 @@
-import { Router } from 'express'
-import { getRanking, importRanking } from './ranking.controller.js'
+import {Router} from 'express'
 import { validateToken } from '../../middlewares/validateToken.js'
+import {getRanking, getRankingByPlayer, importRanking} from './ranking.controller.js'
 
 export const rankingRouter = Router()
 
 rankingRouter.get('/', validateToken, getRanking)
+rankingRouter.get('/pointsByPlayer',validateToken, getRankingByPlayer)
 rankingRouter.post('/import', validateToken, importRanking)
