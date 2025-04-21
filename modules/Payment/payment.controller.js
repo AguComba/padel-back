6d8b85936cd7fc6587fb3dead5f18dd0458729c3
Juan Gonzalez <juanfabri69@hotmail.com>
2025-04-21 19:45:43 -0300

    feat: macro auto task

--- modules/Payment/payment.controller.js
+++ modules/Payment/payment.controller.js
@@ -1,6 +1,8 @@
 import pkg from "pluspagos-aes-encryption"
 import { isAcceptedUser } from "../../middlewares/permisions.js"
 import { PaymentModel } from "./payment.model.js"
+import { MACRO_COMMERCE_ID, MACRO_SECRET, MACRO_FRASE } from "../../config/app.config.js"
+import axios from "axios"
 
 export const payment = async (req, res) => {
   try {
@@ -18,10 +20,8 @@ export const payment = async (req, res) => {
     }
     const { amount, type } = payment
     const { encryptString } = pkg
-    //const secret = "ASOCIACIONPADELCLUB_77b704d8-e759-4d64-bd27-48bf3d95052c"
-    const secret = "ASOCIACIONPADELCLUB_2ebe729a-13b7-414f-b9c9-fea0a392dac7"
-    //const comercio = "8ff45ee1-1acc-4855-bcac-d277fc9a3ff7"
-    const comercio = "4decf2db-3f8c-4060-b975-3b011f60e06d"
+    const secret = MACRO_SECRET
+    const comercio = MACRO_COMMERCE_ID
     const success = "https://apcpadel.com.ar/pagoexitoso"
     const failure = "https://apcpadel.com.ar/pagofallido"
     const sucursal = ""
@@ -66,3 +66,52 @@ export const paymentStatus = async (req, res) => {
     res.status(500).json({ message: "Error en el servidor: " + e.message })
   }
 }
+
+const getToken = async (req, res) => {
+  try{
+    const payload = {
+      guid: MACRO_COMMERCE_ID,
+      frase: MACRO_FRASE
+    }
+    const {data} = await axios.post('https://botonpp.asjservicios.com.ar:8082/v1/sesion', payload, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    return data.data
+  }catch(e){
+    console.log(e)
+    return false
+  }
+}
+
+// Funcion para ejecutar en las tareas automatizadas
+export const paymentTask = async (req, res) => {
+  try{
+    const token = await getToken()
+    console.log(token)
+    const header = `Bearer ${token}`
+    const today = new Date()
+    const yesterday = new Date(today)
+    yesterday.setDate(today.getDate() - 1)
+    const formattedDate = formatDate(yesterday)
+    const data = await axios.get(`https://botonpp.asjservicios.com.ar:8082/v1/transactions?FechaDesde=${formattedDate}&FechaHasta=${formattedDate}&EstadoTransaccion=3`, {
+      headers: {
+        Authorization: header
+      }})
+    if(!data){
+      console.log('No hay pagos para procesar')
+      return
+    }
+    res.status(200).json(data)
+  }catch(e){
+    res.status(500).json({ message: "Error en el servidor: " + e.message })
+  }
+}
+
+const formatDate = (date) => {
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); 
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
