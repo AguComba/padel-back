0c5d3c313c928bff6f7c5d57e3827973613a9dd3
Juan Gonzalez <juanfabri69@hotmail.com>
2025-05-07 21:28:37 -0300

    fix: traer todos los datos del ranking

--- modules/Ranking/ranking.model.js
+++ modules/Ranking/ranking.model.js
@@ -7,7 +7,7 @@ export class RankingModel {
             const currentYear = new Date().getFullYear()
             const queryParams = []
             let query = `
-            SELECT r.id_player, r.points, u.name, u.last_name, cat.name as category, c.name as club, r.status, r.gender
+            SELECT r.id_player, r.points, u.name, u.last_name, cat.name as category, c.name as club, r.status, r.gender, r.id_category
             FROM ranking r
             INNER JOIN players p ON r.id_player = p.id
             INNER JOIN clubs c ON p.id_club = c.id
