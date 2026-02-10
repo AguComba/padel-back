0ab83d63808ac0bd25979a40ca14270aa12395b0
Agustin Comba <acombadev@gmail.com>
2026-02-10 12:35:14 -0300

    se actualiza el nombre de la tabla zone_matches

--- modules/Zones/zone.model.js
+++ modules/Zones/zone.model.js
@@ -1,36 +1,38 @@
 import { executeQuery } from "../../utils/executeQuery.js"
 
 export class ZonesModel {
-  static async getZones(id_tournament, id_category) {
-    const query = `SELECT zm.*, 
+  static async getZones(id_tournament, id_category, isDrop = false) {
+    const query = `SELECT m.*, 
         IFNULL(CONCAT(u1.name, " ", u1.last_name, " - ", u2.name, " ", u2.last_name), 'SIN PAREJA') AS pareja1, 
         IFNULL(CONCAT(u3.name, " ", u3.last_name, " - ", u4.name, " ", u4.last_name), 'SIN PAREJA') AS pareja2,
         cl.name AS club_name, rm.first_set_couple1, rm.first_set_couple2, rm.second_set_couple1, rm.second_set_couple2,
         rm.third_set_couple1, rm.third_set_couple2, rm.winner_couple, rm.wo
-        FROM zones_matches zm
-        LEFT JOIN result_match rm ON rm.id_match = zm.id
-        LEFT JOIN clubs cl ON zm.id_club = cl.id
+        FROM matches m
+        LEFT JOIN result_match rm ON rm.id_match = m.id
+        LEFT JOIN clubs cl ON m.id_club = cl.id
 
-        LEFT JOIN couples c1 ON zm.id_couple1 = c1.id
+        LEFT JOIN couples c1 ON m.id_couple1 = c1.id
         LEFT JOIN players p1 ON c1.id_player1 = p1.id
         LEFT JOIN players p2 ON c1.id_player2 = p2.id
         LEFT JOIN users u1 ON p1.id_user = u1.id
         LEFT JOIN users u2 ON p2.id_user = u2.id 
         
-        LEFT JOIN couples c2 ON zm.id_couple2 = c2.id
+        LEFT JOIN couples c2 ON m.id_couple2 = c2.id
         LEFT JOIN players p3 ON c2.id_player1 = p3.id
         LEFT JOIN players p4 ON c2.id_player2 = p4.id
         LEFT JOIN users u3 ON p3.id_user = u3.id
         LEFT JOIN users u4 ON p4.id_user = u4.id
-        LEFT JOIN tournaments t ON zm.id_tournament = t.id
+        LEFT JOIN tournaments t ON m.id_tournament = t.id
         
-        WHERE zm.id_tournament = ?
-        AND zm.id_category = ?
+        WHERE m.id_tournament = ?
+        AND m.id_category = ?
         AND t.public = 1
-        ORDER BY zm.zone, \`match\``
-    const zones = await executeQuery(query, [id_tournament, id_category])
+        AND m.is_drop = ?
+        ORDER BY m.zone, \`match\``
+    const zones = await executeQuery(query, [id_tournament, id_category, isDrop])
     return zones
   }
+  
     static async saveZones(zones) {
       // Función para formatear los valores y evitar undefined
       const formatZone = (zone) => [
@@ -58,7 +60,7 @@ export class ZonesModel {
           updatePromises.push(
             executeQuery(
               `
-              UPDATE zones_matches
+              UPDATE matches
               SET 
                 id_tournament = ?, 
                 id_category = ?, 
@@ -92,7 +94,7 @@ export class ZonesModel {
       if (inserts.length > 0) {
         await executeQuery(
           `
-          INSERT INTO zones_matches 
+          INSERT INTO matches 
           (id_tournament, id_category, id_couple1, points_couple1, id_couple2, points_couple2, \`match\`, rival1, rival2, zone, day, id_club, hour)
           VALUES ?
         `,
@@ -104,20 +106,20 @@ export class ZonesModel {
     }
   
   static async deleteZones(id_tournament, id_category) {
-    const query = `DELETE FROM zones_matches WHERE id_tournament = ? AND id_category = ?`
+    const query = `DELETE FROM matches WHERE id_tournament = ? AND id_category = ?`
     await executeQuery(query, [id_tournament, id_category])
     return true
   }
   static async searchGeneratedZones(id_tournament, id_category) {
-    const query = `SELECT zm.*, rm.id as idMatch FROM zones_matches zm LEFT JOIN result_match rm ON zm.id = rm.id_match WHERE zm.id_tournament = ? AND zm.id_category = ?`
+    const query = `SELECT m.*, rm.id as idMatch FROM matches m LEFT JOIN result_match rm ON m.id = rm.id_match WHERE m.id_tournament = ? AND m.id_category = ?`
     const zones = await executeQuery(query, [id_tournament, id_category])
     return zones
   }
 
   static async getMatchsByZone(id){
     const query = `  SELECT 
-    z.id_couple1,
-    z.id_couple2,
+    m.id_couple1,
+    m.id_couple2,
     r.first_set_couple1,
     r.first_set_couple2,
     r.second_set_couple1,
@@ -130,22 +132,22 @@ export class ZonesModel {
     IFNULL(CONCAT(u1.name, ' ', u1.last_name, ' - ', u2.name, ' ', u2.last_name), 'SIN PAREJA') AS pareja1,
     IFNULL(CONCAT(u3.name, ' ', u3.last_name, ' - ', u4.name, ' ', u4.last_name), 'SIN PAREJA') AS pareja2
 
-  FROM zones_matches z
-  INNER JOIN result_match r ON z.id = r.id_match
+  FROM matches m
+  INNER JOIN result_match r ON m.id = r.id_match
 
-  LEFT JOIN couples c1 ON z.id_couple1 = c1.id
+  LEFT JOIN couples c1 ON m.id_couple1 = c1.id
   LEFT JOIN players p1 ON c1.id_player1 = p1.id
   LEFT JOIN players p2 ON c1.id_player2 = p2.id
   LEFT JOIN users u1 ON p1.id_user = u1.id
   LEFT JOIN users u2 ON p2.id_user = u2.id
 
-  LEFT JOIN couples c2 ON z.id_couple2 = c2.id
+  LEFT JOIN couples c2 ON m.id_couple2 = c2.id
   LEFT JOIN players p3 ON c2.id_player1 = p3.id
   LEFT JOIN players p4 ON c2.id_player2 = p4.id
   LEFT JOIN users u3 ON p3.id_user = u3.id
   LEFT JOIN users u4 ON p4.id_user = u4.id
 
-  WHERE z.id IN (?)
+  WHERE m.id IN (?)
   AND r.match_type = 'zona'`
     const matchs = await executeQuery(query, [id])
     return matchs
