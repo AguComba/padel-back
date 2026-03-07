49e32834cae05dbab7cd39b542bfd150d874466c
Agustin Comba <acombadev@gmail.com>
2026-03-07 19:35:40 -0300

    se agrega coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vitest/config'
+import {defineConfig} from 'vitest/config'
 
 export default defineConfig({
     test: {
         environment: 'node',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+            include: ['**/*.js'],
+            exclude: [
+                'utils/**',
+                'tests/**',
+                'coverage/**',
+                'node_modules/**',
+                'scripts/**'
+            ],
+        },
         setupFiles: './tests/setup.js',
         include: ["tests/**/*.test.js"],
         testTimeout: 30000,
